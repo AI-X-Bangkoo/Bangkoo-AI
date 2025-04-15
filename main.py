e396b9314595674b9a5ec2ab8cac3690c0169d6f
Beom Seok Kim <abgh012347@gmail.com>
2025-04-15 18:35:40 +0900

    Detection & Masking 작업 추가

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 from api.llmAgent.router import router as recommend_router
 from api.search.router import router as search_router
 from api.placement.router import router as placement_router
+from api.detection.router import router as detection_router
+# from api.detection.sam2_dino_mask_detection_router import router as sam2_dino_mask_detection_router
 from model_loader import model_manager
 from api.llmAgent.router import router as recommend_or_search_router
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+from utils.constants import UPLOAD_DIR
 import threading
 import asyncio
 
 app = FastAPI()
+
+# 2025-04-12 김범석 추가 (static 파일 접근)
+app.mount("/static", StaticFiles(directory=UPLOAD_DIR), name="static")
 
 @app.on_event("startup")
 async def startup_event():
@@ -27,6 +35,8 @@ app.include_router(recommend_router, prefix="/api")
 app.include_router(search_router, prefix="/api")
 app.include_router(recommend_or_search_router, prefix="/api")
 app.include_router(placement_router, prefix="/api")
+app.include_router(detection_router, prefix="/api")
+# app.include_router()
 
 @app.get("/")
 def read_root():
