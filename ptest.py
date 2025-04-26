0d6715b45ab84c0c6c023311089f2320d49e89ee
Kimbang1 <kbhsun123@gmail.com>
2025-04-26 14:25:24 +0900

    dev에서 pull

--- ptest.py
+++ ptest.py
@@ -1,4 +1,7 @@
 import torch
-print(torch.__version__)        # 예: 2.1.0
-print(torch.version.cuda)       # 예: 11.8
-print(torch.cuda.is_available())
+print("PyTorch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA version:", torch.version.cuda)
+print("cuDNN version:", torch.backends.cudnn.version())
+print("GPU name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
+torch._C._dispatch_dump_table()  # 등록된 연산자 확인
